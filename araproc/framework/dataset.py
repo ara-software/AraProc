6858f3f2c10eb5ab9f481e7630d243b8ba73456a
Brian Clark <baclark@umd.edu>
2024-09-26 12:08:09 -0500

    dataset can now load the ttree and set the number of events parameter

--- araproc/framework/dataset.py
+++ araproc/framework/dataset.py
@@ -6,6 +6,26 @@ ROOT.gSystem.Load(os.environ.get('ARA_UTIL_INSTALL_DIR')+"/lib/libAraEvent.so")
 
 class AraDataset:
 
+    """
+    A class for representing an ARA dataset
+
+    ...
+
+    Attributes
+    ----------
+    path_to_root_file : str
+       the full path to the data root file (event data)
+    path_to_pedestal_file : str
+        the full path to the pedestal file to be used to calibrate this data file
+    root_tfile : ROOT TFile
+        the pointer to the ROOT TFile that corresponds to the opened data event file
+    event_tree : 
+        the sound that the animal makes
+    num_events: int
+        number of data events in the data ROOT file
+
+    """
+
     def __init__(self, 
                  path_to_data_file : str,
                  path_to_pedestal_file : str,
@@ -13,8 +33,8 @@ class AraDataset:
           
         self.path_to_root_file = None
         self.path_to_pedestal_file = None
-        self._ttree = None
-        self._root_tfile = None
+        self.root_tfile = None
+        self.event_tree = None
         
         # check if they gave us strings
         if not isinstance(path_to_data_file, str):
@@ -38,18 +58,42 @@ class AraDataset:
         if not os.path.isfile(path_to_pedestal_file):
             raise ValueError(f"{path_to_pedestal_file} looks like a directory, not a file")
         
+        # now that we're sure these are clean, we can assign the variables
         self.path_to_data_file = path_to_data_file
         self.path_to_pedestal_file = path_to_pedestal_file
-        self.open_tfile()
+        
+        # open the file, establish the tree, and its properties
+        self.open_tfile_load_ttree()
+        self.num_events = self.event_tree.GetEntries()
 
-    def open_tfile(self):
+
+    def open_tfile_load_ttree(self):
+
+        # open the TFile
         try:
-            self._root_tfile = ROOT.TFile(self.path_to_data_file, "READ")
+            self.root_tfile = ROOT.TFile(self.path_to_data_file, "READ")
             logging.info(f"Successfully opened {self.path_to_data_file}")
         except:
             logging.critical(f"Opening {self.path_to_data_file} failed")
             raise
 
+        # set the TTree
+        try:
+            self.event_tree = self.root_tfile.Get("eventTree")
+        except:
+            logging.critical("Loading the eventTree failed")
+            self.root_tfile.Close() # close the file
+            raise
 
+        # load up the ARA event
+        self.raw_event_ptr = ROOT.RawAtriStationEvent()
+        try:
+            self.event_tree.SetBranchAddress("event",ROOT.AddressOf(self.raw_event_ptr))
+        except:
+            logging.critical("Assigning the rawEventPtr in the eventTree failed")
+            self.root_tfile.Close() # close the file
+            raise
 
     
+    # def get_calibrated_event(self):
+
