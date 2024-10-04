be0b04b5a1bc6ffade48b442673e85962f688288
Brian Clark <baclark@umd.edu>
2024-10-03 20:36:21 -0500

    single to double quotes

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='araproc',
-    url='https://github.com/clark2688/araproc',
-    author='Brian Clark',
-    author_email='baclark@umd.edu',
-    packages=['araproc'],
-    package_data = {'araproc': ['analysis/data/*']},
+    name="araproc",
+    url="https://github.com/clark2688/araproc",
+    author="Brian Clark",
+    author_email="baclark@umd.edu",
+    packages=["araproc"],
+    package_data = {"araproc": ["analysis/data/*"]},
     include_package_data=True,
-    python_requires='>= 3.9',
-    install_requires=['numpy < 2', 'scipy >= 1.13.0', 'matplotlib', 'pyyaml'],
-    version='0.1',
-    license='GPLv3',
-    description='ARA data analysis framework',
+    python_requires=">= 3.9",
+    install_requires=["numpy < 2", "scipy >= 1.13.0", "matplotlib", "pyyaml"],
+    version="0.1",
+    license="GPLv3",
+    description="ARA data analysis framework",
 )
