4528e5b4864821dc52478332eee8f965ea2b3bc4
Marco Muzio <msm6428@psu.edu>
2024-10-05 00:49:31 -0500

    Created analysis_tools.py and added first functions.

---
+++ araproc/analysis/analysis_tools.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def get_vpp(waveform):
+
+    """
+    Calculates the peak-to-peak voltage of a voltage trace.
+
+    Parameters
+    ----------
+    waveform: TGraph
+        A numpy array of floats containing the voltages for the trace.
+
+    Returns
+    -------
+    vpp : float
+        Peak-to-peak voltage in same units as trace.
+    """ 
+
+    trace = np.asarray(waveform.GetY())
+
+    vMax = trace.max()
+    vMin = trace.min()
+    vpp = vMax - vMin
+
+    return vpp
+
+def get_min_segmented_rms(waveform, nsegs=8):
+
+    """
+    Calculates the RMS of a voltage trace by calculating the RMS of segments
+    voltage trace and returning the minimum value.
+
+    Parameters
+    ----------
+    waveform: TGraph
+        A numpy array of floats containing the voltages for the trace.
+    nsegs: int
+        Number of segments to break waveform into.
+
+    Returns
+    -------
+    rms : float
+        The minimum RMS among all segments in same units as trace.
+    """
+
+    trace = np.asarray(waveform.GetY())
+
+    rms = 1e100
+    traceLen = len(trace)
+    segLen = traceLen // nsegs
+    if(segLen < 2):
+      raise Exception("Number of segments cannot be more than number of points in trace. Abort.")
+
+    segRem = traceLen % nsegs
+
+    for i in range(nsegs):
+
+      start = i*segLen
+      if(i < segRem):
+        start += i
+
+      end = start + segLen
+      if(i < segRem):
+        end += 1
+
+      thisRms = np.sqrt(np.mean(trace[start:end+1]**2))
+
+      if(thisRms < rms):
+        rms = thisRms
+
+    return rms
