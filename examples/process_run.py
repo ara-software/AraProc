2d39e1f86e6d2df56cba54da7481349e272a4d52
Brian Clark <baclark@umd.edu>
2024-10-05 19:03:26 -0500

    add example of how to save things to ROOT files

---
+++ examples/process_run.py
@@ -0,0 +1,66 @@
+import ctypes
+import argparse
+import ROOT
+from array import array
+
+import araproc # noqa: F401
+from araproc.framework import dataset 
+from araproc.analysis import standard_reco as sr
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", type=str,  dest="input_file", required=True,
+    help="full path to the input file")
+parser.add_argument("-t", type=int, default=0, dest="is_simulation", required=True,
+	help="is simulation; 0 = not simulation (default), 1 = is simulation")
+parser.add_argument("-p", type=str,default=None, dest="ped_file", required=False, 
+    help="path to pedestal file")
+parser.add_argument("-s", type=int, dest="station", required=True,
+    help="station id")
+args = parser.parse_args()
+args.is_simulation = bool(args.is_simulation)
+
+if args.is_simulation and (args.ped_file is not None):
+    raise KeyError(f"You cannot mix a simulation with a pedestal file")
+
+if (not args.is_simulation) and (args.ped_file is None):
+    raise KeyError(f"If you are analyzing data, you must provide a pedestal file")
+
+# set up input 
+d = dataset.AnalysisDataset(
+    station_id = args.station,
+    path_to_data_file = args.input_file,
+    path_to_pedestal_file = args.ped_file,
+    is_simulation = args.is_simulation
+)
+reco = sr.StandardReco(d.station_id)
+
+# set up outputs
+f = ROOT.TFile( f"station_{d.station_id}_run_{d.run_number}.root", 
+               "RECREATE")
+tree = ROOT.TTree("results_tree", "results_tree")
+
+event_number = ctypes.c_int()
+tree.Branch("event_number", event_number, "event_number/I")
+
+reco_result_pulser_v = array( "d", [0]*3 )
+tree.Branch("reco_result_pulser_v", reco_result_pulser_v, "reco_result_pulser_v[3]/D")
+
+
+for e in range(0, 20, 1):
+
+    useful_event = d.get_useful_event(e)
+
+    wave_bundle = d.get_waveforms(useful_event)
+    reco_results = reco.do_standard_reco(wave_bundle)
+
+    # stash the output results
+    event_number.value = e
+    reco_result_pulser_v[0] = reco_results["pulser_v"]["corr"]
+    reco_result_pulser_v[1] = reco_results["pulser_v"]["theta"]
+    reco_result_pulser_v[2] = reco_results["pulser_v"]["phi"]
+
+    tree.Fill()
+
+tree.Write()
+f.Close()
