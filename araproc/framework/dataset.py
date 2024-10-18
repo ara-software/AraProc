9a9e4f3867d8f76090fe734156cf4c9e9eeec687
Brian Clark <baclark@umd.edu>
2024-10-18 15:52:24 -0500

    conditional goes the wrong way

--- araproc/framework/dataset.py
+++ araproc/framework/dataset.py
@@ -31,7 +31,7 @@ def get_cvmfs_ped_file_name(station_id, run_number):
     if station_id not in const.valid_station_ids:
         raise Exception(f"Station id {station_id} is not supported")
 
-    if not np.isfinite(run_number) and run_number > 0:
+    if not np.isfinite(run_number) or run_number < 0:
         raise Exception(f"Station id {run_number} is not supported")
     
     cvmfs_top_dir = "/cvmfs/icecube.osgstorage.org/icecube/PUBLIC/groups/arasoft/pedestals"
