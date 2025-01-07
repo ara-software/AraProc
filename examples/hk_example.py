ab804a2ad5d2c60901f1d0e321b2d7f9de5f61b1
Brian Clark <baclark@umd.edu>
2025-01-06 21:55:26 -0600

    wrap up sensor hk example

--- examples/hk_example.py
+++ examples/hk_example.py
@@ -5,33 +5,32 @@ from araproc.framework import housekeeping
 import logging
 logging.getLogger().setLevel(logging.DEBUG)
 
-# for later use
-# d = dataset.AnalysisDataset(
-#     station_id=5,
-#     path_to_data_file="/data/exp/ARA/2019/blinded/L1/ARA05/0701/run005626/event005626.root",
-#     do_not_calibrate = False, # turn this flag to True if you only need raw atri events
-# )
-# iter_start = 0
-# iter_stop = 200
-
+# an example of how to read a sensor Hk file
 shk = housekeeping.SensorHkWrapper(
     path_to_hk_file = "/data/exp/ARA/2019/blinded/L1/ARA05/0701/run005626/sensorHk005626.root"
 )
 iter_start = 0
 iter_stop = shk.num_data_points
 
-
 for p in range(iter_start, iter_stop, 1):
 
-    hk = shk.get_data_point(p)
+    hk = shk.get_sensor_data_point(p)
+
+    # how to get the data point unix time
+    unix_time = hk.unixTime
 
     # how to get dda/tda voltages and currents
     dda_volt = []
     dda_curr = []
+    dda_temp = []
     tda_volt = []
     tda_curr = []
+    tda_temp = []
     for dda in range(4):
         dda_volt.append(hk.getDdaVoltage(dda))
         dda_curr.append(hk.getDdaCurrent(dda))
+        dda_temp.append(hk.getDdaTemp(dda))
         tda_volt.append(hk.getTdaVoltage(dda))
         tda_curr.append(hk.getTdaCurrent(dda))
+        tda_temp.append(hk.getTdaTemp(dda))
+
