c11ebaa6e4519a5dac5881f298f4ef7700da1892
Brian Clark <baclark@umd.edu>
2025-01-06 20:07:54 -0600

    add hk example

---
+++ examples/hk_example.py
@@ -0,0 +1,20 @@
+import araproc # noqa: F401
+from araproc.framework import dataset
+from araproc.framework import housekeeping
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+# for later use
+# d = dataset.AnalysisDataset(
+#     station_id=5,
+#     path_to_data_file="/data/exp/ARA/2019/blinded/L1/ARA05/0701/run005626/event005626.root",
+#     do_not_calibrate = False, # turn this flag to True if you only need raw atri events
+# )
+# iter_start = 0
+# iter_stop = 200
+
+shk = housekeeping.SensorHkWrapper(
+    path_to_hk_file = "/data/exp/ARA/2019/blinded/L1/ARA05/0701/run005626/sensorHk005626.root"
+)
+print("Did the thing")
