674f642ce1cc231579776e9b1fbe8203ca125420
Brian Clark <baclark@umd.edu>
2025-05-28 17:25:19 -0500

    Derive readout lengths from data (#151)
    * remove loading of rf and soft readout info from araproc, and instead derive from data
    
    * don't calculate counts
    
    * remove unnecessary helper function
    
    * even more safety in mode calculation
    
    * simplify again
    
    * use fconsts instead of hardcoded 4

--- araproc/framework/dataset.py
+++ araproc/framework/dataset.py
@@ -6,10 +6,13 @@ import os
 import numpy as np
 import ROOT
 import yaml
+from scipy import stats
+
 
 from araproc.framework import waveform_utilities as wu
 from araproc.framework import constants as const
 from araproc.framework import file_utilities as futil
+from araproc.framework import constants as fconst
 from araproc.analysis import dedisperse as dd
 from araproc.analysis import cw_filter as cwf
 from araproc.analysis.snr import get_snr
@@ -159,6 +162,8 @@ class DataWrapper:
         self.raw_event_ptr = None
         self.cw_id_readers = None
         self.config = None
+        self.__num_rf_readout_blocks = None
+        self.__num_soft_readout_blocks = None
 
         if station_id not in const.valid_station_ids:
             raise Exception(f"Station id {station_id} is not supported")
@@ -273,6 +278,71 @@ class DataWrapper:
     def __assign_config(self):
         self.qual_cuts = ROOT.AraQualCuts.Instance()
         self.config = self.qual_cuts.getLivetimeConfiguration(self.run_number, self.station_id)
+
+    def get_num_rf_readout_blocks(self):
+        if self.__num_rf_readout_blocks is not None:
+            # if this param is alread initiated, then use it
+            return self.__num_rf_readout_blocks
+        else:
+            # otherwise, initialize it
+            self.__establish_readout_params()
+            return self.__num_rf_readout_blocks
+    
+    def get_num_soft_readout_blocks(self):
+        if self.__num_soft_readout_blocks is not None:
+            # if this param is alread initiated, then use it
+            return self.__num_soft_readout_blocks
+        else:
+            # otherwise, initialize it
+            self.__establish_readout_params()
+            return self.__num_soft_readout_blocks
+
+    def __establish_readout_params(self):
+        
+        print("    [LOG] Establishing the Length of RF and Software Blocks....")
+        # Enable only needed branches
+        self.event_tree.SetBranchStatus("*", 0)
+        self.event_tree.SetBranchStatus("event.numReadoutBlocks", 1)
+        self.event_tree.SetBranchStatus("event.triggerInfo[4]", 1)
+
+        total_entries = self.event_tree.GetEntries()
+        numReadoutBlocks_values = np.empty(total_entries, dtype=int)
+        rf_events = np.empty(total_entries, dtype=bool)
+        software_triggers = np.empty(total_entries, dtype=bool)
+
+        # Read all data
+        for i in range(total_entries):
+            self.event_tree.GetEntry(i)
+            
+            # Get numReadoutBlocks directly
+            numReadoutBlocks_values[i] = int(self.event_tree.event.numReadoutBlocks)
+
+            # Get event types
+            event_obj = self.event_tree.event
+            rf_events[i] = event_obj.isRFTrigger()
+            software_triggers[i] = event_obj.isSoftwareTrigger()
+                    
+        # turn all branches back on
+        self.event_tree.SetBranchStatus("*", 1)
+        
+        # Get RF trigger events
+        rf_readout_blocks = numReadoutBlocks_values[rf_events]
+        if len(rf_readout_blocks) > 0:
+            rf_mode = int(stats.mode(rf_readout_blocks).mode)//fconst.num_dda
+        else:
+            rf_mode = int(-10)
+
+        # Get software trigger events  z
+        sw_readout_blocks = numReadoutBlocks_values[software_triggers]
+        if len(sw_readout_blocks) > 0:
+            sw_mode = int(stats.mode(sw_readout_blocks).mode)//fconst.num_dda
+        else:
+            sw_mode = int(-10)
+    
+        # Return results (handle None cases)
+        self.__num_rf_readout_blocks = rf_mode
+        self.__num_soft_readout_blocks = sw_mode
+        print(f"    [LOG] DONE: Length of RF and Software Blocks is {self.__num_rf_readout_blocks}, {self.__num_soft_readout_blocks}")
 
     def __load_pedestal(self, path_to_pedestal_file = None):
 
@@ -1192,6 +1262,17 @@ class AnalysisDataset:
 
         sim_info = self.dataset_wrapper.get_sim_information(event_idx)
         return sim_info
+
+    def get_num_rf_readout_blocks(self):
+        if self.is_simulation:
+            raise ValueError("You requested data information, but this dataset is marked as simulation. Abort!")
+        return self.dataset_wrapper.get_num_rf_readout_blocks()
+
+
+    def get_num_soft_readout_blocks(self):
+        if self.is_simulation:
+            raise ValueError("You requested data information, but this dataset is marked as simulation. Abort!")
+        return self.dataset_wrapper.get_num_soft_readout_blocks()
 
     def get_wavepacket(self,
                       useful_event,
