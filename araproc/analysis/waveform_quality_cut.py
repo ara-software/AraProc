d285d8aee9540607dc75a83a57a8b7390d0ac360
Marco Muzio <msm6428@psu.edu>
2025-12-18 16:16:01 -0600

    Add valid waveform check.

--- araproc/analysis/waveform_quality_cut.py
+++ araproc/analysis/waveform_quality_cut.py
@@ -71,6 +71,55 @@ def check_waveform_saturation(wave_bundle, excluded_channels=[]):
 
     return is_saturated
 
+def check_valid_waveforms(wave_bundle, excluded_channels=[]):
+    """
+    Check the trace values are sensible.
+    
+    Parameters
+    ----------
+    wavepacket : dict
+        A dict with three entries:
+          "event" : int  
+            Event number
+          "waveforms" : dict
+            A dictionary of the 16 waveforms.
+            The key is the RF channel number.
+            The value is a TGraph.
+            There should be 16 entries, even if you don't intend to use all
+            16 traces in your interferometry.
+            The exclusions are handled further down under the excluded channels section.
+          "trace_type" : string
+            Waveform type requested by which_trace
+
+    Returns
+    -------
+    is_invalid : bool
+        Whether the waveform has invalid values. 
+    """
+
+    chans = list(wave_bundle.keys())
+
+    # run check   
+    is_invalid = False
+    for chan in chans:
+        if chan in excluded_channels:
+            continue
+
+        waveform = wave_bundle[chan]
+        time, trace = wfu.tgraph_to_arrays(waveform)
+
+        # check for nans
+        if np.any(np.isnan(time)) or np.any(np.isnan(trace)):
+            is_invalid = True
+            break
+
+        # check for flatlining 
+        if trace.std() == 0.0:
+            is_invalid = True
+            break
+
+    return is_invalid
+
 def is_bad_waveform_quality(wavepacket, excluded_channels=[]):
 
     """
@@ -100,7 +149,7 @@ def is_bad_waveform_quality(wavepacket, excluded_channels=[]):
 
     is_bad_quality = False
 
-    is_bad_quality = is_bad_quality or check_waveform_saturation(wavepacket["waveforms"], excluded_channels)
+    is_bad_quality = is_bad_quality or check_valid_waveforms(wavepacket["waveforms"], excluded_channels)
 
     return is_bad_quality
 
